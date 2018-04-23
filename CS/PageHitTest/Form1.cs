f3de0148cf1ca585e05e91a594a7a56707ca7043
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/PageHitTest/Form1.cs
@@ -0,0 +1,31 @@
+﻿#region #Reference
+using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+using DevExpress.Pdf;
+using DevExpress.XtraBars.Ribbon;
+// ...
+#endregion #Reference
+
+namespace PageHitTest {
+#region #Code
+public partial class Form1 : RibbonForm {
+    public Form1() {
+        InitializeComponent();
+
+        using (Stream stream =
+            Assembly.GetExecutingAssembly().GetManifestResourceStream("PageHitTest.demo.pdf"))
+            pdfViewer.LoadDocument(stream);
+    }
+
+    void pdfViewer_MouseClick(object sender, MouseEventArgs e) {
+        PdfDocumentPosition position = pdfViewer.GetDocumentPosition(e.Location);
+        if (position == null)
+            MessageBox.Show("You clicked outside the page bounds");
+        else
+            MessageBox.Show(String.Format("You clicked on page {0}", position.PageNumber));
+    }
+}
+#endregion #Code
+}
---
+++ VB/PageHitTest/Form1.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic
+#Region "#Reference"
+Imports System
+Imports System.IO
+Imports System.Reflection
+Imports System.Windows.Forms
+Imports DevExpress.Pdf
+Imports DevExpress.XtraBars.Ribbon
+' ...
+#End Region ' #Reference
+
+Namespace PageHitTest
+#Region "#Code"
+Partial Public Class Form1
+	Inherits RibbonForm
+	Public Sub New()
+		InitializeComponent()
+
+		Using stream As Stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("PageHitTest.demo.pdf")
+			pdfViewer.LoadDocument(stream)
+		End Using
+	End Sub
+
+	Private Sub pdfViewer_MouseClick(ByVal sender As Object, ByVal e As MouseEventArgs) Handles pdfViewer.MouseClick
+		Dim position As PdfDocumentPosition = pdfViewer.GetDocumentPosition(e.Location)
+		If position Is Nothing Then
+			MessageBox.Show("You clicked outside the page bounds")
+		Else
+			MessageBox.Show(String.Format("You clicked on page {0}", position.PageNumber))
+		End If
+	End Sub
+End Class
+#End Region ' #Code
+End Namespace
